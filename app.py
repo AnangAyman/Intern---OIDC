2871ba6056c0d45b28b9dee7a003385d20978698
Ayman <anang.ayman@gmail.com>
2025-07-26 14:11:47 +0700

    Prepare for mysql

--- app.py
+++ app.py
@@ -1,11 +1,14 @@
 from website.app import create_app
 from flask_migrate import Migrate
-#import pymysql
+import os
 
 app = create_app({
     'SECRET_KEY': 'secret', # Change secret key later
     'SQLALCHEMY_TRACK_MODIFICATIONS': False,
-    'SQLALCHEMY_DATABASE_URI': 'sqlite:///db.sqlite', # Change uri to pymysql
+    'SQLALCHEMY_DATABASE_URI': (
+        f"mysql+mysqlconnector://{os.environ.get('DB_USER')}:{os.environ.get('DB_PASSWORD')}"
+        f"@{os.environ.get('DB_HOST')}/{os.environ.get('DB_NAME')}"
+    ),
 })
 
 from website.models import db  # Ensure db is imported after the app is created
